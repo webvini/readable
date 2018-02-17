5c963ffdb5b8bfcd3def37e4cf2ecae607383ee9
Vinicius Silva <webvini.silva@gmail.com>
2018-02-16 23:24:26 -0200

    Style rule for description article component

--- app/src/components/article/index.js
+++ app/src/components/article/index.js
@@ -12,11 +12,10 @@ const Article = props =>  {
 
                         <div className="content">
                             <a href="javascript:;" className="category">React</a>
-
                             <h2 className="title"><a href="javascript:;">{article.title}</a></h2>
                             <p>{article.description}</p>
 
-                            <span className="author">por: <a href="javascript:;">Vinicius</a></span>
+                            <span className="about"><a href="javascript:;">Vinicius</a> - 06/06/2017</span>
                         </div>
                     </article>
                 )
