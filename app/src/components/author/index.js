e09e51d12e2383a746976bc28a60640aa48a7d7c
Vinicius Silva <webvini.silva@gmail.com>
2018-02-22 22:19:57 -0300

    Created author component

---
+++ app/src/components/author/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Author = () => {
+    return (
+        <section className="author-wrapper">
+            <div className="inner">
+                <figure></figure>
+                <div>
+                    <a>Vinicius Silva</a>
+                    <span>06/06/2018</span>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Author
--- app/src/components/full-article/index.js
+++ app/src/components/full-article/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import Author from './../author'
+
 const FullArticle = () => {
     return (
         <article className="full-article-wrapper">
-            <h2>Full article</h2>
+            <Author />
+
+            <figure></figure>
+
+            <div className="inner">
+                <h2>Full article</h2>
+            </div>
         </article>
     )
 }
