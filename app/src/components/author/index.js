8eee53966ba1085572b67c9c5494419b7b6c82e5
Vinicius Silva <webvini.silva@gmail.com>
2018-02-24 00:57:08 -0300

    Added icon for author component

--- app/src/components/author/index.js
+++ app/src/components/author/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import moment from 'moment';
 
+import 'font-awesome/css/font-awesome.min.css';
+
 const Author = props => {
     const { author, timestamp } = props
 
     return (
         <section className="author-wrapper">
-            <figure></figure>
+            <figure><i className="fa fa-user-circle"></i></figure>
             <div>
                 <a>{author}</a>
                 <span>{moment(timestamp).fromNow()}</span>
