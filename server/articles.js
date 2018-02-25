c7993eed421f410f492e3cd114f170d537b9260d
Vinicius Silva <webvini.silva@gmail.com>
2018-02-25 20:15:17 -0300

    Added vote method

--- server/articles.js
+++ server/articles.js
@@ -59,7 +59,26 @@ const getAll = token => {
     })
 }
 
+const vote = (token, id, option) => {
+    return new Promise((res) => {
+        let posts = getData(token)
+        post = posts[id]
+        switch(option) {
+            case "upVote":
+                post.voteScore = post.voteScore + 1
+                break
+            case "downVote":
+                post.voteScore = post.voteScore - 1
+                break
+            default:
+                console.log(`posts.vote received incorrect parameter: ${option}`)
+        }
+        res(post)
+    })
+}
+
 module.exports = {
     get,
-    getAll
+    getAll,
+    vote
 }
